6591f6b6196cd47e9ad41135d5e772711bc6bac1
Se77ings <se77ings@gmail.com>
2023-07-10 14:00:59 -0300

    final_update

--- Configs/lib/database/backup/siasb (1).sql
+++ Configs/lib/database/backup/siasb (1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 10/07/2023 às 10:15
+-- Tempo de geração: 10/07/2023 às 10:17
 -- Versão do servidor: 10.4.28-MariaDB
 -- Versão do PHP: 8.0.28
 
