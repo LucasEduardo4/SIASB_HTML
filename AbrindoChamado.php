198e655a38c99ac3d96659e71e8ddeae8517c3a2
Se77ings <se77ings@gmail.com>
2023-08-31 13:59:50 -0300

    goodbye

--- AbrindoChamado.php
+++ AbrindoChamado.php
@@ -3,34 +3,17 @@
 session_start();
 if ($_SERVER["REQUEST_METHOD"] == 'POST') {
     $conn = mysqli_connect("localhost", "root", "", "siasb");
-
-    if (isset($_POST['assunto']) && isset($_POST['descricao']) && isset($_POST['equipamento']) && isset($_POST['categoria'])) {
-        // var_dump($_POST);
+    
+    if (isset($_POST['assunto']) && isset($_POST['descricao']) && isset($_POST['stiID'])) {
         $assunto = $_POST['assunto'];
         $descricao = $_POST['descricao'];
-        $equipamento = $_POST['equipamento'];
-        $categoria = $_POST['categoria'];
         $username = $_SESSION['username'];
-        $existeEquipamento = $_POST['existeEquipamento'];
-        $equipamentoNome = $_POST['equipamentoNome'];
+        $stiID = $_POST['stiID'];
 
         $getUserID = "SELECT IDUsuario FROM TBusuario WHERE nome = '$username'";
         $result = mysqli_query($conn, $getUserID);
         $row = mysqli_fetch_assoc($result);
         $userID = $row['IDUsuario'];
-
-        if ($existeEquipamento == 'true') {
-            $sql1 = "INSERT INTO TBequipamentos(sti_id, descricao, tipo, usuario) VALUES ($equipamento, '" . $equipamentoNome . "', 1, $userID);";
-            $result1 = mysqli_query($conn, $sql1);
-
-            if ($result1) {
-                echo "Equipamento adicionado com sucesso";
-                echo "<br>";
-                echo "\n";
-            } else {
-                echo "Erro ao adicionar equipamento: " . mysqli_error($conn);
-            }
-        }
 
         $datetime = date('Y-m-d H:i:s');
         //verifica se tem imagem:
@@ -49,19 +32,16 @@ if ($_SERVER["REQUEST_METHOD"] == 'POST') {
         $sql = "
         SET @UltimoID = (SELECT MAX(IDChamado) FROM tbchamados);
         SET @UltimoID = IFNULL(@UltimoID, 0) + 1;
-        INSERT INTO tbchamados (IDChamado, assunto, descricao, dataAbertura, status_chamado, responsavel, autor, equipamento, imagem, categoria)
-        VALUES (@UltimoID, '$assunto', '$descricao', '$datetime', 1, null, '$userID', $equipamento, '$imagem_blob', '$categoria');
+        INSERT INTO tbchamados (IDChamado, assunto, descricao, dataAbertura, status_chamado, responsavel, autor, equipamento, imagem)
+        VALUES (@UltimoID, '$assunto', '$descricao', '$datetime', 1, null, '$userID', $stiID, '$imagem_blob');
         ";
         $result = mysqli_multi_query($conn, $sql);
-
         if ($result) {
             echo "Novo chamado adicionado com sucesso!";
         } else {
             echo "Erro ao adicionar chamado: " . mysqli_error($conn);
-            echo "<br>";
             echo "\n";
             echo $sql;
-
         }
     }
 }
