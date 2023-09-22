f661a475d5177ab5e80f6ae5fe1cdae6aef8cfc2
Se77ings <se77ings@gmail.com>
2023-09-22 13:55:17 -0300

    final

--- DetalhandoChamado.php
+++ DetalhandoChamado.php
@@ -50,24 +50,36 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                     // $resultArray['imagem'] = base64_encode($imagem);
                     $resultArray['icone'] = base64_encode($icone);
 
-                    $sql2 = "SELECT imagem from TBimagens WHERE referencia = $IDChamado";
+                    $sql2 = "SELECT imagem from TBimagens WHERE referencia = $IDChamado AND LOG IS NULL";
                     $stmt2 = $conn->prepare($sql2);
                     $stmt2->execute();
                     $result2 = $stmt2->get_result();
 
                     $resultImagens = array();
-                    if ($result->num_rows > 0) {
+                    if ($result2->num_rows > 0) {
                         while ($row2 = $result2->fetch_assoc()) {
                             $imagem = $row2["imagem"];
                             $resultImagens[] = base64_encode($imagem);
                         }
                     }
-                    $resultArray['imagens'] = $resultImagens;
+                    $resultArray['imagensChamado'] = $resultImagens;
                 }
             }
         }
 
+        class Imagem
+        {
+            public $IDImagem;
+            public $dados; // O dado da imagem
+            public $referencia;
 
+            public function __construct($IDImagem, $dados, $referencia)
+            {
+                $this->IDImagem = $IDImagem;
+                $this->dados = $dados;
+                $this->referencia = $referencia;
+            }
+        }
         class LogChamado
         {
             public $IDLog;
@@ -75,42 +87,68 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             public $dataAlteracao;
             public $responsavel;
             public $status;
-            public $imagem;
+            public $imagensLog;
 
-            public function __construct($IDLog, $mensagem, $dataAlteracao, $responsavel, $status, $imagem){
+            public function __construct($IDLog, $mensagem, $dataAlteracao, $responsavel, $status, $imagens){
                 $this->IDLog = $IDLog;
                 $this->mensagem = $mensagem;
                 $this->dataAlteracao = $dataAlteracao;
                 $this->responsavel = $responsavel;
                 $this->status = $status;
-                $this->imagem = $imagem; }
+                $this->imagensLog = $imagens;
+            }
         }
 
-        $sql2 = "SELECT IDLog, mensagem, dataAlteracao, u.nome as 'responsavel', sc.descricao as 'status', referencia, imagem FROM TBLog_chamado
+        $sql2 = "SELECT IDLog, mensagem, dataAlteracao, u.nome as 'responsavel', sc.descricao as 'status', referencia FROM TBLog_chamado
                 left join tbusuario u on responsavel = u.IDUsuario
                 left join tbstatus_chamado sc on status = sc.IDStatus WHERE referencia = $IDChamado";
+                // echo $sql2;
+
         $stmt2 = $conn->prepare($sql2);
         $stmt2->execute();
         $result2 = $stmt2->get_result();
-
         $resultLogs = array();
-        if ($result->num_rows > 0) {
+        if ($result2->num_rows > 0) {
+            $imagens = array(); // Crie um array para armazenar todas as imagens
+
             while ($row2 = $result2->fetch_assoc()) {
+                $sqlImagens = "SELECT * FROM tbimagens WHERE LOG IS NOT NULL AND referencia = " . $row2["IDLog"];
+                $stmtImagens = $conn->prepare($sqlImagens);
+                $stmtImagens->execute();
+                $resultImagens = $stmtImagens->get_result();
+
+                if ($resultImagens->num_rows > 0) {
+                    while ($rowImagem = $resultImagens->fetch_assoc()) {
+                        $imagemencoded = base64_encode($rowImagem["imagem"]);
+                        $imagem = new Imagem(
+                            $rowImagem["IDImagem"],
+                            $imagemencoded,
+                            $rowImagem["referencia"]
+                        );
+                        $imagensLog[] = $imagem;
+                    }
+                }
+
                 $logChamado = new LogChamado(
                     $row2["IDLog"],
                     $row2["mensagem"],
                     $row2["dataAlteracao"],
                     $row2["responsavel"],
                     $row2["status"],
-                    base64_encode($row2["imagem"])
+                    $imagensLog // Passamos o array de objetos Imagem
                 );
                 $resultLogs[] = $logChamado;
+                // echo json_encode($resultLogs);
             }
         }
         $resultArray['logs'] = $resultLogs;
+
         echo json_encode($resultArray);
+        // var_dump($resultArray);
+
     }
 }
+
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     if (isset($_POST['gerarLog'])) {
         // var_dump($_FILES);
@@ -119,22 +157,50 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $mensagem = $_POST['mensagem'];
         $referencia = $_POST['referencia'];
         $responsavel = $_SESSION['username'];
-        if (isset($_FILES['imagem'])) {
-            // Recebendo a imagem
-            $imagem = $_FILES['imagem']['tmp_name'];
-            $imagem_nome = $_FILES['imagem']['name'];
-            $imagem_tipo = $_FILES['imagem']['type'];
+        // if (isset($_FILES['imagem'])) {
+        //     // Recebendo a imagem
+        //     $imagem = $_FILES['imagem']['tmp_name'];
+        //     $imagem_nome = $_FILES['imagem']['name'];
+        //     $imagem_tipo = $_FILES['imagem']['type'];
 
-            // Convertendo a imagem para dados binários (blob)
-            $imagem_blob = addslashes(file_get_contents($imagem));
-        } else {
-            $imagem_blob = null;
+        //     // Convertendo a imagem para dados binários (blob)
+        //     $imagem_blob = addslashes(file_get_contents($imagem));
+        // } else {
+        //     $imagem_blob = null;
+        // }
+        $imagens = array();
+
+        foreach ($_FILES['imagem']['tmp_name'] as $key => $tmp_name) {
+            // Receba a imagem
+            $imagem_temp = $_FILES['imagem']['tmp_name'][$key];
+            $imagem_nome = $_FILES['imagem']['name'][$key];
+            $imagem_tipo = $_FILES['imagem']['type'][$key];
+
+            // Verifique se o arquivo é uma imagem (você pode adicionar validações adicionais)
+            if (strpos($imagem_tipo, 'image') !== false) {
+                // Converta a imagem para dados binários (blob)
+                $imagem_blob = addslashes(file_get_contents($imagem_temp));
+
+                // Armazene os dados da imagem em um array
+                $imagens[] = array(
+                    'nome' => $imagem_nome,
+                    'tipo' => $imagem_tipo,
+                    'blob' => $imagem_blob
+                );
+            }
         }
 
-        $sql = "SET @responsavel = (SELECT IDUsuario FROM tbusuario WHERE nome = '$responsavel');
-        INSERT INTO tblog_chamado(IDLog, mensagem, dataAlteracao, responsavel, status, referencia, imagem)
-        VALUES (NULL, '$mensagem', NOW(), @responsavel, $status, $referencia, '$imagem_blob')";
-        echo $sql;
+        $sql = "SET @UltimoID = (SELECT MAX(IDLog) FROM tblog_chamado);
+        SET @UltimoID = IFNULL(@UltimoID, 0) + 1;
+        SET @responsavel = (SELECT IDUsuario FROM tbusuario WHERE nome = '$responsavel');
+        INSERT INTO tblog_chamado(IDLog, mensagem, dataAlteracao, responsavel, status, referencia)
+        VALUES (NULL, '$mensagem', NOW(), @responsavel, $status, $referencia);";
+        // echo $sql;
+        foreach ($imagens as $imagem) {
+            $sql .= "INSERT INTO tbimagens (imagem, referencia, log) VALUES ('{$imagem['blob']}', @UltimoID, '1');";
+            // 
+        }
+        // echo $sql;
         if (mysqli_multi_query($conn, $sql)) {
             echo "Novo status do chamado adicionado com sucesso!";
         } else {
